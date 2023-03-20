44c540eecac602b658dbd8390f59831148208ad9
KateDorosh <kateupw@gmail.com>
2023-03-20 22:10:03 +0200

    backend render link

--- src/components/CheckoutMain/axios.js
+++ src/components/CheckoutMain/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3200'
+    baseURL: 'https://spa-backend-b62l.onrender.com'
 })
 
 export default instance;
